f1b8e70ae333ce440f0fa9d520f8cbcf3f228410
Rokas <jasinskas.rokas@gmail.com>
2023-06-10 16:03:01 +0300

    Added player class

--- warcardsgame.py
+++ warcardsgame.py
@@ -14,3 +14,37 @@ class Card:
 suits[2]
 
 
+class Deck:
+   def __init__ (self): 
+     self.all_cards = []
+
+     for suit in suits: 
+       for rank in ranks: 
+         self.all_cards.append(Card(suit, rank))
+
+  def shuffle(self): 
+    random.shuffle (self.all_cards)
+
+class Player:
+  def __init__(self, name): 
+    self.name = name 
+    self.all_cards = []
+
+  def remove_card(self):
+    return self.all_cards.pop(0)
+  
+  def add_card(self, new_cards)
+    if type(new_cards) == type ([]):
+      self.all_cards.extend(new_cards)
+    else: 
+      self.all_cards.append (new_cards)
+
+  def __str__(self): 
+    return f'Player {self.name} has {len(self.all_cards)} cards'
+  
+  
+
+      
+
+  
+
