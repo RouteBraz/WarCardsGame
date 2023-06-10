e2fd0783838908c6a3b919476860722b73d9bc82
Ruta Braz <rutos.kazlauskaites@gmail.com>
2023-06-10 15:40:07 +0300

    Adding first class

--- warcardsgame.py
+++ warcardsgame.py
@@ -1,3 +1,16 @@
 import random
+suits = ("H", "D", "S", "C")
+ranks = ("A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2" )
+
 class Card:
-  pass
+
+  def __init__(self, suit, rank):
+    self.suit = suit
+    self.rank = rank
+
+  def __str__(self):
+    return f'{self.rank} of {self.suit}'
+
+suits[2]
+
+
