ffcee92f15e5966ae7a51335f5d383c06e475ad5
Grant Innes <129431946+nonProphet-90@users.noreply.github.com>
2023-06-13 13:30:05 +0300

    updates towar loop, indentation & other minor changes commented in file

--- warcardsgame.py
+++ warcardsgame.py
@@ -4,14 +4,10 @@ import sys
 def main():
 
   suits = ("H", "D", "S", "C")
-  ranks = ("A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2" )
-
-   print("Hello, let's start the WAR game")
-   get_player_names()
-   start_the_game(name1, name2)
-
-
-
+  ranks = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2" ] # changed to list to work as tuple of strings to work with add_cards method argument
+  print("Hello, let's start the WAR game")
+  name1, name2 = get_player_names()  # assigned name1/2
+  start_the_game(name1, name2, suits, ranks)   # added suits/ranks arguments
 
 #create a single card
 class Card:
@@ -30,7 +26,8 @@ class Card:
 
 # create a deck of cards with possibility to shuffle
 class Deck:
-  def __init__ (self): 
+
+  def __init__ (self, suits, ranks):   #indentation changed
     self.all_cards = []
 
     for suit in suits: 
@@ -43,7 +40,7 @@ class Deck:
 
 # remove one card from the list of all cards  
   def deal_card(self):
-        return self.cards.pop()
+    return self.all_cards.pop()     #indentation changed
 
 
 #create a player with no cards, that removes and adds up the cards
@@ -56,7 +53,7 @@ class Player:
   def remove_card(self):
     return self.player_cards.pop(0)
   
-  def add_cards(self, new_cards):
+  def add_cards(self, *new_cards):  # changed argument to include additional, card 1 & card 2 args
     if type(new_cards) == type ([]):
       self.player_cards.extend(new_cards)
     else: 
@@ -78,7 +75,7 @@ def get_player_names():
   player2 = input("Enter the name of the second player: ")
   return player1, player2
 
-def start_the_game(name1, name2):
+def start_the_game(name1, name2, suits, ranks):  # added suits/ranks arguments
 
   # introduce the players
   player_one = Player(name1)
@@ -86,12 +83,12 @@ def start_the_game(name1, name2):
 
 
   # create and shuffle the deck
-  new_deck = Deck()
+  new_deck = Deck(suits, ranks) # added args
   new_deck.shuffle()
 
 
   # split the deck between players
-  for i in range(len(new_deck.all_cards)/2):
+  for i in range(len(new_deck.all_cards)//2):  # changed operator to // as error given regarxding float
     player_one.add_cards(new_deck.deal_card())
     player_two.add_cards(new_deck.deal_card())
 
@@ -103,60 +100,72 @@ def start_the_game(name1, name2):
   while game:
     round += 1
      
-  # check the cards number of each player
-  # announce the winner if any doesn't have cards
-  if len(player_one.all_cards) == 0:
-    game = False
-    print(f"{player_one.name} is out of cards")
-    print(f"{player_two.name} is the winner!")
-    print("GAME OVER")
-    sys.exit()
+    # check the cards number of each player
+    # announce the winner if any doesn't have cards
+    if len(player_one.player_cards) == 0:
+      game = False
+      print(f"{player_one.name} is out of cards")
+      print(f"{player_two.name} is the winner!")
+      print("GAME OVER")
+      sys.exit()
 
-  elif len(player_two.all_cards) == 0:
-    game = False
-    print(f"{player_two.name} is out of cards")
-    print(f"{player_one.name} is the winner!")
-    print("GAME OVER")
-    sys.exit()
+    elif len(player_two.player_cards) == 0:
+      game = False
+      print(f"{player_two.name} is out of cards")                   # Fixed indentation within while loop
+      print(f"{player_one.name} is the winner!")
+      print("GAME OVER")
+      sys.exit()
 
-  #else game continues  
-  else:
-    print(f"{round} round:")
-
-    card1 = player1.player_card.pop()
-    card2 = player2.player_card.pop()
-
-    print(f"{player_one.name} plays: {card1}")
-    print(f"{player_two.name} plays: {card2}")
-
-    if ranks.index(card1.rank) < ranks.index(card2.rank):
-      player_one.add_cards(card1, card2)
-      print(f"{player_one.name} wins the round")
-    
-    elif ranks.index(card1.rank) > ranks.index(card2.rank):
-      player_two.add_cards(card1, card2)
-      print(f"{player_two.name} wins the round")
-
-
-  # if both cards are equal, announce the war
+    #else game continues  
     else:
-      # add 3 cards form each palyer to war_cards list
-      war_cards = []
-      for _ in range(3):
-        war_cards.append(player_one.remove_card())
-        war_cards.append(player_two.remove_card())
+      print(f"{round} round:")
 
-      # continue by playing card from each player
-      war_card1 = player_one.remove_card()
-      war_card2 = player_two.remove_card()
+      card1 = player_one.player_cards.pop()   # changed from player1 to player_one
+      card2 = player_two.player_cards.pop()    # same change for player 2
 
-      print(f"{player_one.name} plays: {war_card1}")
-      print(f"{player_two.name} plays: {war_card2}")
+      print(f"{player_one.name} plays: {card1}")
+      print(f"{player_two.name} plays: {card2}")
+
+      if ranks.index(card1.rank) < ranks.index(card2.rank):
+        player_one.add_cards(card1, card2)
+        print(f"{player_one.name} wins the round")
+      
+      elif ranks.index(card1.rank) > ranks.index(card2.rank):
+        player_two.add_cards(card1, card2)
+        print(f"{player_two.name} wins the round")
+
+# implemented war option if tie
+    # if both cards are equal, announce the war
+      else:
+        # add 3 cards form each palyer to war_cards list
+        war_cards = []
+        for _ in range(3):
+          war_cards.append(player_one.remove_card())
+          war_cards.append(player_two.remove_card())
+
+        # continue by playing card from each player
+        war_card1 = player_one.remove_card()
+        war_card2 = player_two.remove_card()
+
+        print(f"{player_one.name} plays: {war_card1}")
+        print(f"{player_two.name} plays: {war_card2}")
+
+        if ranks.index(war_card1.rank) < ranks.index(war_card2.rank):
+          player_one.add_cards(war_cards + [card1, card2, war_card1, war_card2])
+          print(f"{player_one.name} wins the war")
+        elif ranks.index(war_card1.rank) > ranks.index(war_card2.rank):
+          player_two.add_cards(war_cards + [card1, card2, war_card1, war_card2])
+          print(f"{player_two.name} wins the war")
+        else:
+          # If there is another war, add the cards to the war_cards list and continue the loop
+          war_cards.extend([card1, card2, war_card1, war_card2])
 
 
 
-  
-
+'''
+Still a few things to do for the changes i made to actually work, 
+will update branch after finishing current prohjects i am working on
+'''
 
 
 
